9963b7134c1312739003badd51d95922cb3d724f
Nityanshi-Sharma-0103 <nityanshisharma0103@gmail.com>
2024-09-23 11:08:11 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "niti@google.com"
+var accountPassword = "12345"
+accountCity = "Indore"
+let accountState;
+
+//accountId = 2//
+
+accountEmail = "ns@google.com"
+accountPassword = "2121212121"
+accountCity = "Mumbai"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 + 3) // we are using nodejs not browser
+
+let name = "Niti"
+let age = 21
+let isLoggedIn = false
+let state;
+
+
+/*
+numbers => 2 power 53
+bigint
+string = ""
+boolean => false / true
+null => standalone value
+undefined = empty
+symbol => unique
+
+*/
+
+
+// object
+
+console.log(typeof"Nityanshi");
+
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof null);
+console.log(typeof undefined);
+
