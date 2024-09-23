730bb5550cb3fdd228c5e392bc35dad36c5ef20e
Nityanshi-Sharma-0103 <nityanshisharma0103@gmail.com>
2024-09-23 11:31:53 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score = "Niti"
+
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(valueInNumber);
+
+/*
+"33" => 33
+"33abc" >= NaN
+true >= 1, false => 0
+*/
+
+let isLoggedIn = "Niti"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+/*
+1 => true
+0 => false
+"" => false
+"Niti" => true
+*/
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
