a295997fdfbc76fb40bc530ef4d74b7d7a11ec19
Nityanshi-Sharma-0103 <nityanshisharma0103@gmail.com>
2024-09-29 06:29:29 +0000

    datatype-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,39 @@
+// Primitive datatype :
+
+// string, number, boolean, null, undefined, symbol, BigInt
+
+
+/*const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const OutsideTemp = null;
+let UserEmail;
+
+
+const id = symbol('123');
+const anotherId = symbol('123'); 
+*/
+
+
+
+const BigInt = 232455686432689964220n
+//console.log(BigInt);
+
+// Reference (Non primitive) :
+
+// Array, Object, Function
+
+const Heroes =["shaktiman","naagraj","doga"];
+
+let myObj = {
+    name: "Nityanshi",
+    age: 22,
+}
+
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myFunction);
